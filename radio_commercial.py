005dec02b790e1c29e914aa314a31507674d68f1
Owen <owensmith321@gmail.com>
2021-09-25 17:27:44 -0400

    Update radio_commercial.py

--- radio_commercial.py
+++ radio_commercial.py
@@ -1,14 +1,19 @@
-
-
-
 N, P = map(int,input().split())
-commercials = list(map(lambda x: int(x)-P, input().split()))
-dp = [0 for _ in range(len(commercials))]
+C = list(map(lambda x: int(x)-P, input().split())) # commerical breaks
 
-if (all(c < 0 for c in commercials)): # if all negative take max
-   print(max(commercials))
+if (all(c < 0 for c in C)): # if all negative take max
+   print(max(C))
 
-elif (all(c > 0 for c in commercials)): # if all profit take the whole array
-   print(sum(commercials))
+elif (all(c > 0 for c in C)): # if all profit take the whole array
+   print(sum(C))
 
-   
+# our recurrence then looks as follows
+# S(i) = { C[i] if i=0, max{S(i-1) + C[i], C[i]} }
+# where S(i) is the maximum profit over all subsequences ending at i. 
+# where C is our list of commerical breaks
+# The lhs is adding C[i] to the subsequence, the rhs is starting a new one
+S = [C[0]] 
+for i in range(1,len(C)):
+   S.append(max(S[i-1]+C[i],C[i]))
+print(max(S))
+      
